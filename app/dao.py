fd7a72039507f96470f34dc4435d41f80459dde9
thanhvu080803@gmail.com <thanhvu080803@gmail.com>
2023-10-30 09:56:25 +0700

    init project

---
+++ app/dao.py
@@ -0,0 +1,42 @@
+def get_categories():
+    return [{
+        "id":1,
+        "name":"Mobile"
+    },{
+        "id": 2,
+        "name": "Tablet"
+    }]
+
+def get_products():
+    products = [{
+        "id":1,
+        "name": "iPhone 13",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id":2,
+        "name": "Nokia",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id":3,
+        "name": "SamSung",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id":4,
+        "name": "OPPO",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id":5,
+        "name": "iPhone 15",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    },{
+        "id":6,
+        "name": "iPhone XS",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    }]
+    return products
