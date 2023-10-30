9129f5ea358528b0ddff1ef6d6497c45c0192438
thanhvu080803@gmail.com <thanhvu080803@gmail.com>
2023-10-30 21:15:36 +0700

    app

--- app/dao.py
+++ app/dao.py
@@ -1,42 +1,61 @@
 def get_categories():
     return [{
-        "id":1,
-        "name":"Mobile"
-    },{
+        "id": 1,
+        "name": "Mobile"
+    }, {
         "id": 2,
         "name": "Tablet"
     }]
 
-def get_products():
+def get_products(kw):
     products = [{
-        "id":1,
+        "id": 1,
         "name": "iPhone 13",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    },{
-        "id":2,
+    }, {
+        "id": 2,
         "name": "Nokia",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    },{
-        "id":3,
+    }, {
+        "id": 3,
         "name": "SamSung",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    },{
-        "id":4,
+    }, {
+        "id": 4,
         "name": "OPPO",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    },{
-        "id":5,
+    }, {
+        "id": 5,
         "name": "iPhone 15",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
-    },{
-        "id":6,
+    }, {
+        "id": 6,
+        "name": "iPhone XS",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    }, {
+        "id": 7,
+        "name": "iPhone XS",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    }, {
+        "id": 8,
+        "name": "iPhone XS",
+        "price": 20000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
+    }, {
+        "id": 9,
         "name": "iPhone XS",
         "price": 20000000,
         "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg"
     }]
+
+    if kw:
+        products = [p for p in products if p['name'].find(kw) >= 0]
+
     return products
--- app/index.py
+++ app/index.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dao
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    kw = request.args.get('kw')
     cates = dao.get_categories()
-    prods = dao.get_products()
+    prods = dao.get_products(kw)
 
     return render_template('index.html', categories=cates, products = prods)
 
